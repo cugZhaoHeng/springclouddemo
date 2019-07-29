720fa58852775f7fd4a4d7e27a436aa1dcc7dca3
Administrator <zhaoheng2017@126.com>
2019-07-29 16:17:00 +0800

    增加了Hibernate模块

---
+++ quantity-train-hebernate/src/main/java/com/cug/quantitytrainhebernate/QuantityTrainHebernateApplication.java
@@ -0,0 +1,13 @@
+package com.cug.quantitytrainhebernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantityTrainHebernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantityTrainHebernateApplication.class, args);
+    }
+
+}
---
+++ quantity-train-hebernate/src/test/java/com/cug/quantitytrainhebernate/QuantityTrainHebernateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cug.quantitytrainhebernate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuantityTrainHebernateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
